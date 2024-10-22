d72d231e9aa19403043e81c37af490fa8ee4dab3
Feliperr0 <felquev@gmail.com>
2024-10-22 00:20:18 -0600

    cambios

--- router/cities.js
+++ router/cities.js
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import City from '../models/City.js'
 import allCities from "../controllers/cities/read.js";
 
 const router = Router()
 
-router.get('/all', allCities
-
-)
+router.get('/all', allCities)
 
 
 export default router
