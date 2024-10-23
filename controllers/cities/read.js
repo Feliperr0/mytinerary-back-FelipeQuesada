fb70134bee159b0102634a1313d762db1edde84e
Familia QM <felqueu@gmail.com>
2024-10-22 22:31:26 -0600

    se solucionaron problemas en en endpoint de citiesFilter para la pagina Cities.jsx

--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -8,10 +8,39 @@ let allCities = async (req, res) => {
 
         })
     } catch (error) {
-       next(error)
+        next(error)
     }
 }
 
+let citiesFilter = async (req, res, next) => {
+    try {
+        let { country, continent, city } = req.query;
+        console.log('Country:', country);
+        console.log('Continent:', continent);
+        console.log('City:', city);
+
+        let query = {};
+
+        if (country) {
+            query.country = { $regex: '^' + country, $options: 'i' };
+        }
+
+        if (continent) {
+            if (Array.isArray(continent)) {
+                query.continent = { $in: continent.map(c => new RegExp(c, 'i')) };
+            } else {
+                query.continent = { $regex: continent, $options: 'i' };
+            }
+        }
+
+       
+
+        let all = await City.find(query);
+        return res.status(200).json({ cities: all });
+    } catch (error) {
+        next(error);
+    }
+};
 
 
 
@@ -28,8 +57,8 @@ const getCityById = async (req, res) => {
         }
         res.status(200).json(city);
     } catch (error) {
-       next (error)
+        next(error)
     }
 };
 
-export { allCities, getCityById }
+export { allCities, getCityById, citiesFilter }
--- router/cities.js
+++ router/cities.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import {allCities, getCityById} from "../controllers/cities/read.js";
+import {allCities, getCityById, citiesFilter} from "../controllers/cities/read.js";
 
 const router = Router()
 
 router.get('/all', allCities)
 
 router.get('/id/:id', getCityById);
+
+router.get('/find', citiesFilter)
 
 
 export default router
