b02bfe47e7ddcadd1309fe4113a36a80d9156870
Familia QM <felqueu@gmail.com>
2024-11-04 14:14:33 -0600

    se hicieron los endpoints controladores de itineraries

--- controllers/cities/update.js
+++ controllers/cities/update.js
@@ -1,20 +1,14 @@
 import City from "../../models/City.js";
 
 const updateCity = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const updatedData = req.body;
-        const updatedCity = await City.findByIdAndUpdate(id, updatedData, { new: true });
+  try {
+    const updatedData = req.body;
+    const updatedCity = await City.findByIdAndUpdate(req.params.id, updatedData, { new: true });
 
-        if (!updatedCity) {
-            return res.status(404).json({ message: 'City not found' });
-        }
-
-        res.status(200).json(updatedCity);
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json(updatedCity);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { updateCity };
-
--- models/City.js
+++ models/City.js
@@ -1,20 +1,18 @@
 import { Schema, model } from 'mongoose';
 
-
 let collection = 'cities';
-
 let schema = new Schema({
-  country: { type: String, required: true },
-  city: { type: String, required: true },
+  country: { type: String, required: true, index: true },
+  city: { type: String, required: true, index: true },
   photo: { type: String, required: true },
   attractions: { type: [String], required: true },
   description: { type: String, required: true },
   localCurrency: { type: String, required: true },
   continent: { type: String, required: true },
-  itinerary: [{ type: Schema.Types.ObjectId, ref: 'Itinerary', required: true }]
+  itinerary: [{ type: Schema.Types.ObjectId, ref: 'Itinerary', required: false }]
 }, {
   timestamps: true
 });
 
 let City = model(collection, schema);
-export default City;
+export default City
