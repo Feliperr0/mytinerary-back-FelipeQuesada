eb5ef9007d408fd5aa2c36f5c44b9a97c2ab44ac
Feliperr0 <felquev@gmail.com>
2024-11-26 19:27:10 -0600

    actualizacion google log

--- controllers/auth/signinGoogle.js
+++ controllers/auth/signinGoogle.js
@@ -9,7 +9,7 @@ export default async (req, res, next) => {
         console.log(req)
         console.log("TOKEN", req.token)
 
-        return res.redirect('http://localhost:5173/cities/?token=' + req.token + '')
+        return res.redirect('https://frontend-aa9cd.web.app/cities/?token=' + req.token + '')
 
     } catch (error) {
         next(error)
